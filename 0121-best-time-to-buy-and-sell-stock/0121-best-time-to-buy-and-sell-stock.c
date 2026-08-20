ddc130d10f88f8c03b122acc7bba337b01db221b
ghost of tsushima <samargarg98@gmail.com>
2026-08-20 15:54:03 +0530

    Time: 0 ms (100%), Space: 16 MB (51.66%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,21 @@
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        // Update the minimum buying price
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        // Calculate profit if we sell today
+        int profit = prices[i] - minPrice;
+
+        // Update maximum profit
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+}
